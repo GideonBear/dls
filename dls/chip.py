d3991637a23c23316ea62615989452795abfaa41
Gideon <87426140+GideonBear@users.noreply.github.com>
2023-01-27 11:26:48 +0100

    Add better dataclass representation

--- dls/chip.py
+++ dls/chip.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import dataclasses
 from collections.abc import Sequence
 from dataclasses import dataclass
+from operator import attrgetter
 from typing import TYPE_CHECKING, Literal
 
 
@@ -9,8 +11,20 @@ if TYPE_CHECKING:
     from typing_extensions import Self
 
 
-@dataclass(eq=False)
-class Chip:
+class BetterRepr:
+    def __repr__(self) -> str:
+        nodef_f_vals = (
+            (f.name, attrgetter(f.name)(self))
+            for f in dataclasses.fields(self)
+            if attrgetter(f.name)(self) != f.default
+        )
+
+        nodef_f_repr = ", ".join(f"{name}={value}" for name, value in nodef_f_vals)
+        return f"{self.__class__.__name__}({nodef_f_repr})"
+
+
+@dataclass(eq=False, repr=False)
+class Chip(BetterRepr):
     name: str
     inputs: Sequence[Pin] | int
     outputs: Sequence[Pin] | int
@@ -27,8 +41,8 @@ class Chip:
         return cls('SIGNAL OUT', [pin], [])
 
 
-@dataclass(eq=False)
-class Pin:
+@dataclass(eq=False, repr=False)
+class Pin(BetterRepr):
     # implicit-optional doesn't work?
     chip: Chip | None = None
     index: int | None = None
